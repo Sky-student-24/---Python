d8d3f95750825554d8168e967efb7cb7cf10c565
Sky-student-24 <storm1sniper@gmail.com>
2026-03-19 17:35:06 +0300

    Поправки

--- 07_lesson/Pages/CalculatorPage.py
+++ 07_lesson/Pages/CalculatorPage.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class CalculatorPage:
@@ -17,6 +19,9 @@ class CalculatorPage:
         self._driver.find_element(By.XPATH, "//span[text()='+']").click()
         self._driver.find_element(By.XPATH, "//span[text()='8']").click()
         self._driver.find_element(By.XPATH, "//span[text()='=']").click()
+
+        wait = WebDriverWait(self._driver, 45)
+        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "div.screen"), "15"))
 
     def results(self):
         return self._driver.find_element(By.CSS_SELECTOR, "div.screen").text
--- 07_lesson/test_calculator.py
+++ 07_lesson/test_calculator.py
@@ -1,21 +1,15 @@
 from selenium import webdriver
 from Pages.CalculatorPage import CalculatorPage
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_calculator():
     driver = webdriver.Chrome()
 
     calc_page = CalculatorPage(driver)
-    calc_page.search("45")  # Выставление задержки
-    calc_page.push_numbers()  # Ввод чисел
+    calc_page.search("45")
+    calc_page.push_numbers()
 
-    wait = WebDriverWait(driver, 45)  # Ждем результата
-    wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "div.screen"), "15"))
-
-    result = calc_page.results()  # Результат вычисления
+    result = calc_page.results()
     assert result == "15"
 
     driver.quit
