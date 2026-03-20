184e46017e3114f74b6c802a05539ad7eae17626
Sky-student-24 <storm1sniper@gmail.com>
2026-03-20 18:01:33 +0300

    2 поправка

--- 07_lesson/test_calculator.py
+++ 07_lesson/test_calculator.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from Pages.CalculatorPage import CalculatorPage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_calculator():
@@ -8,6 +11,9 @@ def test_calculator():
     calc_page = CalculatorPage(driver)
     calc_page.search("45")
     calc_page.push_numbers()
+
+    wait = WebDriverWait(driver, 45)
+    wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "div.screen"), "15"))
 
     result = calc_page.results()
     assert result == "15"
